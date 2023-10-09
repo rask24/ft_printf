ee471591426c9ad703bc0491dd227b52393a8bce
rask24 <rask0x18@gmail.com>
2023-10-09 12:38:02 +0900

    fix parse precision

--- srcs/parse_spec.c
+++ srcs/parse_spec.c
@@ -6,7 +6,7 @@
 /*   By: reasuke <reasuke@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/03 14:17:21 by reasuke           #+#    #+#             */
-/*   Updated: 2023/10/05 01:16:31 by reasuke          ###   ########.fr       */
+/*   Updated: 2023/10/09 12:00:44 by reasuke          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,8 +56,6 @@ static void	parse_precision(t_format_spec *fs, t_format_result *fr, va_list *ap)
 		if (*fr->format == '*')
 		{
 			fs->precision = va_arg(*ap, int);
-			if (fs->precision < 0)
-				fs->precision = PREC_NONE;
 			fr->format++;
 		}
 		else
@@ -66,6 +64,8 @@ static void	parse_precision(t_format_spec *fs, t_format_result *fr, va_list *ap)
 			while (ft_isdigit(*fr->format))
 				fr->format++;
 		}
+		if (fs->precision < 0)
+			fs->precision = PREC_NONE;
 	}
 }
 
